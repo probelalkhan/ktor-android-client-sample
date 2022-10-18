7bc38bb72a848791500422866fa915657a09b6a5
Belal Khan <belal.haque@yulu.bike>
2022-10-18 22:09:52 +0530

    UI Changes

--- app/src/main/java/net/simplifiedcoding/tmdbmovies/ui/movies/MovieItem.kt
+++ app/src/main/java/net/simplifiedcoding/tmdbmovies/ui/movies/MovieItem.kt
@@ -1,7 +1,6 @@
 package net.simplifiedcoding.tmdbmovies.ui.movies
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,11 +13,15 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import net.simplifiedcoding.tmdbmovies.R
 import net.simplifiedcoding.tmdbmovies.ui.theme.AppTheme
 import net.simplifiedcoding.tmdbmovies.ui.theme.spacing
@@ -49,15 +52,20 @@ fun MovieItem() {
                 .fillMaxWidth()
         ) {
 
-            Image(
-                painter = painterResource(id = R.drawable.bg_jurassic_world),
-                contentDescription = "Jurassic World",
-                modifier = Modifier.weight(0.3f)
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(R.drawable.bg_jurassic_world)
+                    .crossfade(true)
+                    .build(),
+                placeholder = painterResource(R.drawable.bg_image_placeholder),
+                contentDescription = "",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.weight(0.4f)
             )
 
             Column(
                 modifier = Modifier
-                    .weight(0.7f)
+                    .weight(0.6f)
                     .padding(start = spacing.medium)
             ) {
                 Text(
@@ -66,17 +74,17 @@ fun MovieItem() {
                     color = MaterialTheme.colorScheme.onSurface
                 )
 
-                Spacer(modifier = Modifier.size(spacing.small))
+                Spacer(modifier = Modifier.size(spacing.medium))
 
                 Text(
                     text = "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history’s most fearsome creatures.",
                     style = MaterialTheme.typography.bodyMedium,
                     color = MaterialTheme.colorScheme.onSurface,
-                    maxLines = 3,
+                    maxLines = 7,
                     overflow = TextOverflow.Ellipsis
                 )
 
-                Spacer(modifier = Modifier.size(10.dp))
+                Spacer(modifier = Modifier.size(spacing.medium))
 
                 Text(
                     text = "IMDB 8.9",
